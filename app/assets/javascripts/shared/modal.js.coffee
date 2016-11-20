805f008794bed282a0be5192315a50aedb8525ee
Arnel T. Lenteria <arnel.lenteria@gmail.com>
2016-11-20 11:51:15 +0800

    done prototype

--- app/assets/javascripts/shared/modal.js.coffee
+++ app/assets/javascripts/shared/modal.js.coffee
@@ -4,6 +4,10 @@ $(document).on 'click', '.modal-trigger', (e)->
   target.modal('show')
 
 $(document).on 'click', '.dismiss-call-prompt', ->
-  dismissed_tickets = localStorage.getItem('dismissed_call_prompts') || []
+  dismissed_tickets = localStorage.getItem('dismissed_call_prompts') || ""
+  dismissed_tickets = dismissed_tickets.split(",")
+  if typeof dismissed_tickets == 'string' || typeof dismissed_tickets == 'number'
+    dismissed_tickets = [dismissed_tickets]
+
   dismissed_tickets.push $(this).data('id')
   localStorage.setItem('dismissed_call_prompts', dismissed_tickets)
--- app/controllers/api/tickets_controller.rb
+++ app/controllers/api/tickets_controller.rb
@@ -2,11 +2,11 @@ class Api::TicketsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def index
     if params[:customer_phone].present? && params[:customer_name].present?
-      ticket = Ticket.create(status: 'OPEN', is_new: true, title: 'Request a call', customer_phone: params[:customer_phone], customer_name: params[:customer_name])
+      ticket = Ticket.create(status: 'OPEN', is_new: true, title: 'Request a call', customer_phone: params[:customer_phone], customer_name: params[:customer_name], category: params[:category])
     end
   end
 
   def check_for_new
-    @new_ticket = Ticket.where(is_new: true).first
+    @new_tickets = Ticket.where(is_new: true)
   end
 end
